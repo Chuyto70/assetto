3ba7df3ce4befcc131f79847da84293218625650
Sygix <733267-Sygix@users.noreply.gitlab.com>
2023-05-04 16:25:48 +0200

    fix: rename rootlayout from slugLayout

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -38,7 +38,7 @@ export async function generateMetadata({
   return metadata;
 }
 
-export default function SlugLayout({
+export default function RootLayout({
   children,
   params: { lang },
 }: {
