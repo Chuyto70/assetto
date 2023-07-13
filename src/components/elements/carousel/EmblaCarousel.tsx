7ffed559fcd5c7666cd6a9f7a534b5a61b4b5a42
Sygix <733267-Sygix@users.noreply.gitlab.com>
2023-07-13 23:40:30 +0200

    feat: carousel section WIP

--- src/components/elements/carousel/EmblaCarousel.tsx
+++ src/components/elements/carousel/EmblaCarousel.tsx
@@ -7,19 +7,50 @@ import { ReactNode } from "react";
 
 import clsxm from '@/lib/clsxm';
 
-const EmblaCarousel = (props: { children: ReactNode; className?: string; options?: Partial<OptionsType>; autoplay?: boolean; autoplayOptions?: unknown }) => {
+const EmblaCarousel = (props: {
+  children: ReactNode;
+  className?: string;
+  containerClassName?: string;
+  options?: Partial<OptionsType>;
+  autoplay?: boolean;
+  autoplayOptions?: unknown;
+  thumbsChildren?: ReactNode[];
+  thumbsClassName?: string;
+  thumbsContainerClassName?: string;
+  thumbClassName?: string;
+  thumbOptions?: Partial<OptionsType>;
+}) => {
 
   const autoplay = props.autoplay ? [Autoplay(props.autoplayOptions ?? {})] : [];
-  const [emblaRef] = useEmblaCarousel(props.options ?? {}, autoplay);
+  const [emblaRef, emblaApi] = useEmblaCarousel(props.options ?? {}, autoplay);
+  const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel(props.thumbOptions ?? {})
 
   return (
-    <div ref={emblaRef}
-      className={clsxm('overflow-hidden', props.className)}
-    >
-      <div className="flex">
-        {props.children}
+    <>
+      <div ref={emblaRef}
+        className={clsxm('overflow-hidden', props.className)}
+      >
+        <div className={clsxm('flex', props.containerClassName)}>
+          {props.children}
+        </div>
       </div>
-    </div>
+
+      {props.thumbsChildren && <div
+        ref={emblaThumbsRef}
+        className={clsxm('overflow-hidden', props.thumbsClassName)}
+      >
+        <div className={clsxm('flex', props.thumbsContainerClassName)}>
+          {props.thumbsChildren.map((thumb, index) => (
+            <button key={index}
+              className={clsxm('flex-shrink-0 flex-grow-0', props.thumbClassName)}
+              onClick={() => null}
+            >
+              {thumb}
+            </button>
+          ))}
+        </div>
+      </div>}
+    </>
   )
 }
 
--- src/components/sections/Carousel.tsx
+++ src/components/sections/Carousel.tsx
@@ -66,37 +66,70 @@ const Carousel = async (props: { pageID: number; index: number }) => {
   const { page: { data: { attributes: { content } } } }: dataType = await QueryContentComponent(locale, props.pageID, 'page', 'page', ComponentSectionsCarousel, 'sectionsCarousel');
   const { items } = content[props.index];
 
+  const thumbs = items.map((item) => (
+    <div key={item.id}
+      className="relative w-full h-full"
+    >
+      <Image
+        className="h-full w-full object-center object-cover brightness-75"
+        width={item.image.data.attributes.width}
+        height={item.image.data.attributes.height}
+        src={MediaUrl(item.image.data.attributes.url)}
+        alt={item.image.data.attributes.alternativeText}
+        sizes="100vw (min-width: 768px) 70vw"
+      />
+      <h3 className="absolute bottom-0 left-0 w-full line-clamp-2 text-center p-3 lg:p-6 text-sm lg:text-base text-primary-600">
+        {item.title}
+      </h3>
+    </div>
+  ));
+
   return (
-    <section className="mx-3">
-      <EmblaCarousel
-        options={{ loop: true, containScroll: 'trimSnaps', }}
-        autoplay={false}
-        autoplayOptions={{
-          delay: 5000,
-          stopOnInteraction: false,
-        }}
-      >
-        {items.map((item) => (
-          <CarouselItem key={item.id}
-            className="relative w-full aspect-square xs:aspect-video rounded-3xl overflow-hidden mr-3"
-          >
-            <Image
-              className="h-full w-full object-center object-cover brightness-75"
-              width={item.image.data.attributes.width}
-              height={item.image.data.attributes.height}
-              src={MediaUrl(item.image.data.attributes.url)}
-              alt={item.image.data.attributes.alternativeText}
-              sizes="100vw (min-width: 768px) 70vw"
-            />
-            <div className="absolute bottom-0 left-0 p-3 text-white no-underline">
-              {item.href ? <Link href={item.href}>
-                <MDXRemote source={item.description} />
-              </Link> : <MDXRemote source={item.description} />}
-            </div>
-          </CarouselItem>
-        ))}
-      </EmblaCarousel>
-    </section>
+    <section className="w-full px-3 lg:px-6 max-w-screen-2xl">
+      <div className="relative w-full flex flex-col md:flex-row gap-3 lg:gap-6">
+        <EmblaCarousel
+          className="w-full md:w-3/4 md:pr-3 lg:pr-6 shrink-0"
+          containerClassName="w-full"
+          options={{ loop: true, containScroll: 'trimSnaps', }}
+          autoplay={false}
+          autoplayOptions={{
+            delay: 5000,
+            stopOnInteraction: false,
+          }}
+          thumbsChildren={thumbs}
+          thumbOptions={{
+            containScroll: 'keepSnaps',
+            dragFree: true,
+            breakpoints: {
+              '(min-width: 768px)': { axis: 'y' }
+            }
+          }}
+          thumbsClassName="md:absolute right-0 md:w-1/4 md:h-full"
+          thumbsContainerClassName="md:flex-col md:h-full"
+          thumbClassName="aspect-video w-1/2 xs:w-1/3 md:w-full md:max-h-1/4 rounded-3xl overflow-hidden mr-3 last:mr-0 md:mr-0 md:mb-3 lg:mb-6 md:last:mb-0"
+        >
+          {items.map((item) => (
+            <CarouselItem key={item.id}
+              className="relative w-full aspect-square xs:aspect-video rounded-3xl overflow-hidden mr-3 lg:mr-6"
+            >
+              <Image
+                className="h-full w-full object-center object-cover brightness-75"
+                width={item.image.data.attributes.width}
+                height={item.image.data.attributes.height}
+                src={MediaUrl(item.image.data.attributes.url)}
+                alt={item.image.data.attributes.alternativeText}
+                sizes="100vw (min-width: 768px) 70vw"
+              />
+              <div className="absolute bottom-0 left-0 p-3 lg:p-6 text-white no-underline">
+                {item.href ? <Link href={item.href}>
+                  <MDXRemote source={item.description} />
+                </Link> : <MDXRemote source={item.description} />}
+              </div>
+            </CarouselItem>
+          ))}
+        </EmblaCarousel>
+      </div>
+    </section >
   )
 }
 
