a8aee60226f09587d0f07bc2ccdc7d7336b7866a
Sygix <733267-Sygix@users.noreply.gitlab.com>
2023-07-31 17:47:55 +0200

    feat(MediaCarousel): add complete design + new component to create carouselbtn

--- src/components/sections/MediaCarousel.tsx
+++ src/components/sections/MediaCarousel.tsx
@@ -4,6 +4,7 @@ import { gql, QueryContentComponent } from "@/lib/graphql";
 import { MediaUrl } from "@/lib/helper";
 import { Media } from "@/lib/interfaces";
 
+import CarouselBtn from "@/components/elements/carousel/CarouselBtn";
 import CarouselItem from "@/components/elements/carousel/CarouselItem";
 import EmblaCarousel from "@/components/elements/carousel/EmblaCarousel";
 import NextImage from "@/components/NextImage";
@@ -17,6 +18,7 @@ const ComponentSectionsMediaCarousel = gql`
       data {
         id
         attributes {
+          name
           slug
           media {
             data {
@@ -91,15 +93,15 @@ const MediaCarousel = async (props: { pageID: number; index: number }) => {
           }}
         >
           {medias.data.map((media, index) => {
-            const { ext_video, thumbnail, slug, media: uploadFile } = media.attributes;
+            const { ext_video, thumbnail, slug, media: uploadFile, name } = media.attributes;
 
             if ((ext_video && thumbnail.data) || (uploadFile.data?.attributes.mime.startsWith('video/') && thumbnail.data)) return (
               <CarouselItem key={media.id}
                 index={index}
-                className="w-2/3 relative h-fit"
-                nxSelectedClassName="z-10"
-                wrapperClassNames="relative left-1/2 -translate-x-1/2 w-[110%] h-fit aspect-video scale-90 transition-transform duration-300 after:bg-carbon-900/60 after:transition-colors after:w-full after:h-full after:absolute after:inline-block after:top-0 after:left-0 after:rounded-2xl"
-                nxSelectedClassNames="scale-100 after:bg-transparent"
+                className="w-2/3 relative h-fit group/item"
+                nxSelectedClassName="z-10 is-selected"
+                wrapperClassNames="relative left-1/2 -translate-x-1/2 w-[110%] h-fit aspect-video scale-90 transition-transform duration-300 [&>a]:after:bg-carbon-900/60 [&>a]:after:transition-colors [&>a]:after:w-full [&>a]:after:h-full [&>a]:after:absolute [&>a]:after:inline-block [&>a]:after:top-0 [&>a]:after:left-0 [&>a]:after:rounded-2xl"
+                nxSelectedClassNames="scale-100 [&>a]:after:bg-transparent"
                 nX={0}
               >
                 <Link href={`/${locale}/media/${slug}`}
@@ -116,11 +118,43 @@ const MediaCarousel = async (props: { pageID: number; index: number }) => {
                     sizes='90vw (min-width: 1280px) 60vw'
                   />
                 </Link>
+                <div className="transition-opacity duration-300 opacity-0 group-[.is-selected]/item:opacity-100 mt-3 md:mt-6 flex justify-center items-center flex-nowrap px-3">
+                  <CarouselBtn icon="material-symbols:arrow-left-alt-rounded" className="text-carbon-700 dark:text-carbon-400 hover:text-carbon-900 dark:hover:text-carbon-200 transition-colors w-full flex justify-end" iconClassName="w-6 h-6 md:w-8 md:h-8" />
+                  {name && <h3 className="shrink-0 max-w-full px-3 text-center text-sm md:text-lg font-normal text-carbon-700 dark:text-carbon-400">{name}</h3>}
+                  <CarouselBtn isNext={true} icon="material-symbols:arrow-right-alt-rounded" className="text-carbon-700 dark:text-carbon-400 hover:text-carbon-900 dark:hover:text-carbon-200 transition-colors w-full flex justify-start" iconClassName="w-6 h-6 md:w-8 md:h-8" />
+                </div>
               </CarouselItem>
             );
 
             else if (uploadFile.data?.attributes.mime.startsWith('image/')) return (
-              <></>
+              <CarouselItem key={media.id}
+                index={index}
+                className="w-2/3 relative h-fit group/item"
+                nxSelectedClassName="z-10 is-selected"
+                wrapperClassNames="relative left-1/2 -translate-x-1/2 w-[110%] h-fit aspect-video scale-90 transition-transform duration-300 [&>a]:after:bg-carbon-900/60 [&>a]:after:transition-colors [&>a]:after:w-full [&>a]:after:h-full [&>a]:after:absolute [&>a]:after:inline-block [&>a]:after:top-0 [&>a]:after:left-0 [&>a]:after:rounded-2xl"
+                nxSelectedClassNames="scale-100 [&>a]:after:bg-transparent"
+                nX={0}
+              >
+                <Link href={`/${locale}/media/${slug}`}
+                  scroll={false}
+                  className="relative z-10 pointer-cursor"
+                >
+                  <NextImage
+                    className='w-full h-full'
+                    imgClassName='rounded-2xl w-full h-full object-cover object-center'
+                    width={uploadFile.data.attributes.width}
+                    height={uploadFile.data.attributes.height}
+                    src={MediaUrl(uploadFile.data.attributes.url)}
+                    alt={uploadFile.data.attributes.alternativeText ?? ''}
+                    sizes='90vw (min-width: 1280px) 60vw'
+                  />
+                </Link>
+                <div className="transition-opacity duration-300 opacity-0 group-[.is-selected]/item:opacity-100 mt-3 md:mt-6 flex justify-center items-center flex-nowrap px-3">
+                  <CarouselBtn icon="material-symbols:arrow-left-alt-rounded" className="text-carbon-700 dark:text-carbon-400 hover:text-carbon-900 dark:hover:text-carbon-200 transition-colors w-full flex justify-end" iconClassName="w-6 h-6 md:w-8 md:h-8" />
+                  {name && <h3 className="shrink-0 max-w-full px-3 text-center text-sm md:text-lg font-normal text-carbon-700 dark:text-carbon-400">{name}</h3>}
+                  <CarouselBtn isNext={true} icon="material-symbols:arrow-right-alt-rounded" className="text-carbon-700 dark:text-carbon-400 hover:text-carbon-900 dark:hover:text-carbon-200 transition-colors w-full flex justify-start" iconClassName="w-6 h-6 md:w-8 md:h-8" />
+                </div>
+              </CarouselItem>
             );
 
             else return null;
@@ -129,7 +163,52 @@ const MediaCarousel = async (props: { pageID: number; index: number }) => {
       </div>}
 
 
-      {/* Less than 1 item only centered image */}
+      {/* Less than 3 item only centered image */}
+      {medias.data.length < 3 && <div className="px-3 md:px-6 lg:px-12 w-full max-w-screen-xl flex justify-center gap-3 md:gap-6">
+        {medias.data.map((media) => {
+          const { ext_video, thumbnail, slug, media: uploadFile, name } = media.attributes;
+
+          if ((ext_video && thumbnail.data) || (uploadFile.data?.attributes.mime.startsWith('video/') && thumbnail.data)) return (
+            <Link href={`/${locale}/media/${slug}`}
+              key={media.id}
+              scroll={false}
+              className="w-1/2 pointer-cursor"
+            >
+              <NextImage
+                className="w-full aspect-video"
+                imgClassName="rounded-2xl object-center object-cover w-full h-full"
+                width={thumbnail.data.attributes.width}
+                height={thumbnail.data.attributes.height}
+                src={MediaUrl(thumbnail.data.attributes.url)}
+                alt={thumbnail.data.attributes.alternativeText ?? ''}
+                sizes='90vw (min-width: 1280px) 60vw'
+              />
+              {name && <h3 className="mt-3 md:mt-6 text-center text-sm md:text-lg font-normal text-carbon-700 dark:text-carbon-400">{name}</h3>}
+            </Link>
+          );
+
+          else if (uploadFile.data?.attributes.mime.startsWith('image/')) return (
+            <Link href={`/${locale}/media/${slug}`}
+              key={media.id}
+              scroll={false}
+              className="w-1/2 pointer-cursor"
+            >
+              <NextImage
+                className="w-full aspect-video"
+                imgClassName="rounded-2xl object-center object-cover w-full h-full"
+                width={uploadFile.data.attributes.width}
+                height={uploadFile.data.attributes.height}
+                src={MediaUrl(uploadFile.data.attributes.url)}
+                alt={uploadFile.data.attributes.alternativeText ?? ''}
+                sizes='90vw (min-width: 1280px) 60vw'
+              />
+              {name && <h3 className="mt-3 md:mt-6 text-center text-sm md:text-lg font-normal text-carbon-700 dark:text-carbon-400">{name}</h3>}
+            </Link>
+          );
+
+          else return null;
+        })}
+      </div>}
     </section>
   )
 }
