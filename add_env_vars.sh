51a79ad198ecbf8ff92a2b8be20fbf83a1a29bdb
Sygix <733267-Sygix@users.noreply.gitlab.com>
2023-08-14 23:07:31 +0200

    fix: test

--- add_env_vars.sh
+++ add_env_vars.sh
@@ -7,23 +7,18 @@ env_file=".env.production"
 add_env_vars() {
   # Loop through each key in the .env.production file
   for key in $(grep -E '^[A-Za-z_][A-Za-z0-9_]*=' "$env_file" | sed -E 's/=.*//'); do
-    echo "Processing key: $key"
     # Get the value currently set in the .env.production file
     value=$(grep "^$key=" "$env_file" | sed -E "s/^$key=//")
     
-    echo "Current value for $key: $value"
-    
     # Check if the value is empty (not set)
     if [ -z "$value" ]; then
-      echo "Value is empty for $key, checking environment variable..."
       # Get the value of the environment variable with the same name
       env_value=$(printenv "$key")
       
       # Check if the environment variable is set
       if [ -n "$env_value" ]; then
-        echo "Setting value for $key from environment variable: $env_value"
-        # Update or add the environment variable in the .env.production file
-        sed -i "s/^$key=.*/$key=$env_value/" "$env_file"
+        # Add the environment variable to the .env.production file
+        echo "$key=$env_value" >> "$env_file"
       fi
     fi
   done
